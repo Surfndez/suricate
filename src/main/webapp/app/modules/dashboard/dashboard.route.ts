f2a863ece90891e1311357f3f1e50da57d31c5a2
Axel Chesné <axel.chesnes_ext@michelin.com>
2019-04-29 10:52:52 +0200

    #215 - add spaces before first import

--- src/main/webapp/app/modules/dashboard/dashboard.route.ts
+++ src/main/webapp/app/modules/dashboard/dashboard.route.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import {Routes} from '@angular/router';
 
 import {DashboardTvComponent} from './pages/dashboard-tv/dashboard-tv.component';
